d35eefddd29fd6cc666e8ee8ff393712cb4750c4
taraneh2abd <taraneh.abdellahi2@gmail.com>
2023-12-19 17:28:37 +0330

    updated main

--- main.py
+++ main.py
@@ -13,14 +13,19 @@ data_list = []
 unique_tokens_in_all = set()
 index = 0
 while path.isfile(CONTENT_FILE_PATTERN.format(index = index)):
-
+    sentences = []
     filename = CONTENT_FILE_PATTERN.format(index = index)
     file_path = CONTENT_DIR / CONTENT_FILE_PATTERN.format(index=index)
+
     with open(file_path, 'r', encoding='utf-8') as file:
         content = file.read()
+        for line in file:
+            sentences.append(set(tokenizer(line.strip())))
+
     tokens = tokenizer(content)
 
     unique_tokens_in_all |= set(tokens) 
 
-    data_list.append({'Filename': filename, 'unique Tokens of document': tokens, 'Content': content})
+    data_list.append({'Filename': filename, 'unique Tokens of document': tokens, 'Content': content,
+                      'unique Tokens of sentence': sentences})
     index += 1
