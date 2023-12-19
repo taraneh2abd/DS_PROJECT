11e356c85bc8a73cd891c69f74ed601fd546c6c7
taraneh2abd <taraneh.abdellahi2@gmail.com>
2023-12-19 17:56:35 +0330

    created dataset generator

---
+++ dataset_generator.py
@@ -0,0 +1,29 @@
+import json
+from pathlib import path
+import pandas as pd
+from word_tokenizer import tokenizer 
+
+def dataset_format_converter(CONTENT_FILE_PATTERN, CONTENT_DIR):
+    data_list = []
+    unique_tokens_in_all = set()
+    index = 0
+    while path.isfile(CONTENT_FILE_PATTERN.format(index = index)):
+        sentences = []
+        filename = CONTENT_FILE_PATTERN.format(index = index)
+        file_path = CONTENT_DIR / CONTENT_FILE_PATTERN.format(index=index)
+
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            for line in file:
+                sentences.append(set(tokenizer(line.strip())))
+
+        tokens = tokenizer(content)
+
+        unique_tokens_in_all |= set(tokens) 
+
+        data_list.append({'Filename': filename, 'unique Tokens of document': tokens, 'Content': content,
+                        'unique Tokens of sentence': sentences})
+        index += 1
+
+
+    return data_list , unique_tokens_in_all 
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 from pathlib import path
 import pandas as pd
 from word_tokenizer import tokenizer 
+from dataset_generator import dataset_format_converter
 
 JSON_FILE = path("dataset") / "data.json"
 CONTENT_FILE_PATTERN = "document_{index}.txt"
@@ -9,23 +10,4 @@ CONTENT_DIR = path("dataset/data")
 
 query = json.loads(open(JSON_FILE, encoding="UTF8").read())
 
-data_list = []
-unique_tokens_in_all = set()
-index = 0
-while path.isfile(CONTENT_FILE_PATTERN.format(index = index)):
-    sentences = []
-    filename = CONTENT_FILE_PATTERN.format(index = index)
-    file_path = CONTENT_DIR / CONTENT_FILE_PATTERN.format(index=index)
-
-    with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.read()
-        for line in file:
-            sentences.append(set(tokenizer(line.strip())))
-
-    tokens = tokenizer(content)
-
-    unique_tokens_in_all |= set(tokens) 
-
-    data_list.append({'Filename': filename, 'unique Tokens of document': tokens, 'Content': content,
-                      'unique Tokens of sentence': sentences})
-    index += 1
+data_list , unique_tokens_in_all = dataset_format_converter(CONTENT_FILE_PATTERN, CONTENT_DIR)
