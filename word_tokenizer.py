e234f7ab40ffad5a4568b015d488cc2ab2d5010a
taraneh2abd <taraneh.abdellahi2@gmail.com>
2023-12-19 15:59:18 +0330

    Created word_tokenizer

---
+++ word_tokenizer.py
@@ -0,0 +1,22 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+nltk.download('stopwords')
+nltk.download('punkt')
+stopwords = set(stopwords.words("english"))
+
+def remove_punctuation(text):
+    cleaned_text = re.sub(r'[^\w\s]', '', text)
+    return cleaned_text
+
+def tokenizeer(sentence):
+    lemmatizer = WordNetLemmatizer()
+
+    stopwords = set(stopwords.words("english"))
+    lemmatizer = WordNetLemmatizer()
+    sentence = remove_punctuation(sentence)
+    tokens = word_tokenize(sentence.lower())  
+    tokens = set(lemmatizer.lemmatize(token) for token in tokens if token not in stopwords)
+    return list(tokens)
